499eb8ccd2428aad2c75b32d719da119e6a985fa
Rabia Ausaf <rabia.codes@gmail.com>
2026-08-30 21:48:20 +0500

    Refactor input validation and division logic

--- calculator/script.js
+++ calculator/script.js
@@ -9,47 +9,46 @@ const divBtn = document.querySelector('#divide');
 const result = document.querySelector('#result');
 
 addBtn.addEventListener('click', function() {
-  let x = Number(num1.value);
-  let y = Number(num2.value);
-
-  if (num1.value === ' ' || num2.value === ' ') {
+  if (num1.value === '' || num2.value === '') {
     result.innerText = "Please enter both numbers!";
   } else {
+    let x = Number(num1.value);
+    let y = Number(num2.value);
     result.innerText = "Result: " + (x + y);
   }
 });
 
 subBtn.addEventListener('click', function() {
-  let x = Number(num1.value);
-  let y = Number(num2.value);
-
-  if (num1.value === ' ' || num2.value === ' ') {
+  if (num1.value === '' || num2.value === '') {
     result.innerText = "Please enter both numbers!";
   } else {
+    let x = Number(num1.value);
+    let y = Number(num2.value);
     result.innerText = "Result: " + (x - y);
   }
 });
 
 mulBtn.addEventListener('click', function() {
-  let x = Number(num1.value);
-  let y = Number(num2.value);
-
-  if (num1.value === ' ' || num2.value === ' ') {
+  if (num1.value === '' || num2.value === '') {
     result.innerText = "Please enter both numbers!";
   } else {
+    let x = Number(num1.value);
+    let y = Number(num2.value);
     result.innerText = "Result: " + (x * y);
   }
 });
 
 divBtn.addEventListener('click', function() {
-  let x = Number(num1.value);
-  let y = Number(num2.value);
-
-  if (num1.value === ' ' || num2.value === ' ') {
+  if (num1.value === '' || num2.value === '') {
     result.innerText = "Please enter both numbers!";
-  } else if (y === 0) {
-    result.innerText = "Cannot divide by zero!";
   } else {
-    result.innerText = "Result: " + (x / y);
+    let x = Number(num1.value);
+    let y = Number(num2.value);
+    
+    if (y === 0) {
+      result.innerText = "Cannot divide by zero!";
+    } else {
+      result.innerText = "Result: " + (x / y);
+    }
   }
 });
